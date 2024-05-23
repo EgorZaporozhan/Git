dfdb5e173dc15002e86649ae153c6dc4cf13eafd
EgorZaporozhan <149730174+EgorZaporozhan@users.noreply.github.com>
2024-05-23 20:47:49 +0300

    Add files via upload

---
+++ 6Home/6Home/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+
+{
+    static void Main()
+
+    {
+
+        string sentence = "Це приклад речення яке складається з десяти різних слів клас";
+
+
+
+        string[] words = sentence.Split(' ');
+
+
+
+        if (words.Length == 10)
+
+        {
+            string[] wordArray = new string[10];
+            for (int i = 0; i < words.Length; i++)
+            {
+
+                wordArray[i] = words[i];
+
+            }
+
+
+
+            for (int i = 0; i < wordArray.Length; i++)
+            {
+                Console.WriteLine(wordArray[i]);
+
+            }
+
+        }
+
+        else
+        {
+
+            Console.WriteLine("Речення не містить рівно 10 слів.");
+
+        }
+
+    }
+
+}
